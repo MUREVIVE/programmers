976f1c6899de1350be126eb671a4586f8fbd4b8d
MUREVIVE <51858239+MUREVIVE@users.noreply.github.com>
2020-10-31 14:03:28 +0900

    deque 풀이로 다시 분석

--- algorithm/level_2/프린터.py
+++ algorithm/level_2/프린터.py
@@ -21,19 +21,27 @@ def solution(priorities, location):
 ###################################################################################
 
 # 다른 풀이
-
+## - deque 사용법 - popleft(), append
+## - deque 내에서 enumerate 사용법 
+from collections import deque
 def solution(priorities, location):
-  answer = 0
-  from collections import deque
+    answer = 0
+    
+    # enumerate : 인덱스 번호와 컬렉션의 원소를 tuple형태로 반환
+    d = deque([(value,i) for i,value in enumerate(priorities)])
+    while len(d):
+        # d의 맨 앞 제거
+        item = d.popleft() # ex : (2, 0) / (1, 1) / (3, 2) / (2, 3) -> (1, 1) / (3, 2) / (2, 3)
+        # print(max(d)[0]) 
+        print(d)
+        if len(d) != 0 and max(d)[0] > item[0]:
+            # d의 맨 뒤에 추가
+            d.append(item) # ex : (1, 1) / (3, 2) / (2, 3) -> (1, 1) / (3, 2) / (2, 3) / (2, 0)
+        else:
+            answer += 1
+            if item[1] == location:
+                break
+                
+    return answer
 
-  d = deque([(v,i) for i,v in enumerate(priorities)])
 
-  while len(d):
-      item = d.popleft()
-      if d and max(d)[0] > item[0]:
-          d.append(item)
-      else:
-          answer += 1
-          if item[1] == location:
-              break
-  return answer
